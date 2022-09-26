bd5ab004fc99a8819071de8167397ef616a59486
Adam Hemming <hemming.dev@gmail.com>
2022-09-26 22:03:03 +0900

    make header with basic positioning

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,16 @@
-const Header = () => {
+import '../styles/header.css';
 
+const Header = () => {
+  return(
+  <header>
+      <h1>Concentration</h1>
+      <div className='scores'>
+        <p>Moves: 0</p>
+        <p>Best Score: 0</p>
+      </div>
+    <p className='note'>(Lower is better!)</p>
+  </header>
+  )
 }
 
 export default Header;
