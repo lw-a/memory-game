135926dc586aa5bf0a8cc9acec0bbcbc33e45ba1
Adam Hemming <hemming.dev@gmail.com>
2022-09-27 15:17:45 +0900

    create shuffle function

--- src/App.js
+++ src/App.js
@@ -3,6 +3,33 @@ import Card from './components/Card';
 import './styles/app.css';
 
 function App() {
+
+  let cardContent = [1, 2, 3, 4, 5, 6]
+
+  function shuffle(array) {
+
+    let doubled = [];
+
+    array.forEach((item) => {
+      for (let i = 0; i < 2; i++) {
+        doubled.push(item);
+      }
+    });
+
+    const shuffled = [];
+
+    let length = doubled.length;
+    while (length) {
+      let i = Math.floor(Math.random() * doubled.length);
+      if (i in doubled) {
+        shuffled.push(doubled[i]);
+        delete doubled[i];
+        length--;
+      }
+    }
+    return shuffled;
+  }
+
   return (
     <div className="App">
       <Header />
